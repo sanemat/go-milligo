521bd871224177b591b7546e41b1ce540715dfeb
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-05-03 22:27:52 +0700

    feat(main): exit 42

--- cmd/milligo/main.go
+++ cmd/milligo/main.go
@@ -1,4 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Print("(module\n")
+	fmt.Print("    (import \"wasi_unstable\" \"proc_exit\" (func $proc_exit (param i32)))\n")
+	fmt.Print("    (memory 1)\n")
+	fmt.Print("    (export \"memory\" (memory 0))\n")
+	fmt.Print("    (func $main (export \"_start\")\n")
+	fmt.Print("        (call $proc_exit\n")
+	fmt.Print("            (i32.const 42)\n")
+	fmt.Print("        )\n")
+	fmt.Print("    )\n")
+	fmt.Print(")\n")
 }
