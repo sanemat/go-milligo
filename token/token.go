37b869a9f0a395d2a4607325ae86ff074f6dabed
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-05-07 22:19:12 +0700

    wip

---
+++ token/token.go
@@ -0,0 +1,16 @@
+package token
+
+type Kind string
+
+type Token struct {
+	Kind Kind
+	Next *Token
+	Val  float32
+	Str  *string
+}
+
+const (
+	RESERVED = "RESERVED"
+	NUM      = "NUM"
+	EOF      = "EOF"
+)
