406708296f5f7ca7c7c8730c0d2da8e02be543be
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2020-05-09 16:45:47 +0700

    refactor(ast): traverse ast

--- cmd/milligo/main.go
+++ cmd/milligo/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/sanemat/go-milligo/parser"
 	"github.com/sanemat/go-milligo/token"
 	"os"
 	"strconv"
@@ -91,6 +92,78 @@ func tokenize() (*token.Token, error) {
 	return head.Next, nil
 }
 
+//
+// Parser
+//
+
+func newNode(kind parser.NodeKind) parser.Node {
+	return parser.Node{
+		Kind: kind,
+	}
+}
+
+func newBinary(kind parser.NodeKind, lhs *parser.Node, rhs *parser.Node) *parser.Node {
+	node := newNode(kind)
+	node.LHS = lhs
+	node.RHS = rhs
+	return &node
+}
+
+func newNum(val int) *parser.Node {
+	node := newNode(parser.NUM)
+	node.Val = val
+	return &node
+}
+
+// expr = num ("+" num | "-" num)*
+func expr() *parser.Node {
+	n, err := expectNumber()
+	if err != nil {
+		fmt.Fprint(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+	node := newNum(n)
+	for {
+		if consume("+") {
+			n, err := expectNumber()
+			if err != nil {
+				fmt.Fprint(os.Stderr, err.Error())
+				os.Exit(1)
+			}
+			node = newBinary(parser.ADD, node, newNum(n))
+		} else if consume("-") {
+			n, err := expectNumber()
+			if err != nil {
+				fmt.Fprint(os.Stderr, err.Error())
+				os.Exit(1)
+			}
+			node = newBinary(parser.SUB, node, newNum(n))
+		} else {
+			return node
+		}
+	}
+}
+
+//
+// Code generator
+//
+func gen(node *parser.Node) {
+	if node.Kind == parser.NUM {
+		fmt.Printf("            i32.const %d\n", node.Val)
+		return
+	}
+
+	gen(node.LHS)
+	gen(node.RHS)
+
+	switch node.Kind {
+	case parser.ADD:
+		fmt.Print("            i32.add\n")
+	case parser.SUB:
+		fmt.Print("            i32.sub\n")
+	}
+}
+
 var err2 error
 
 func main() {
@@ -99,12 +172,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Tokenize and parse
 	userInput = os.Args[1]
 	tk, err2 = tokenize()
 	if err2 != nil {
 		fmt.Fprint(os.Stderr, err2.Error())
 		os.Exit(1)
 	}
+	node := expr()
 
 	fmt.Print("(module\n")
 	fmt.Print("    (import \"wasi_unstable\" \"proc_exit\" (func $proc_exit (param i32)))\n")
@@ -113,41 +188,9 @@ func main() {
 	fmt.Print("    (func $main (export \"_start\")\n")
 	fmt.Print("        (call $proc_exit\n")
 
-	// The first token must be a number
-	n, err := expectNumber()
-	if err != nil {
-		fmt.Fprint(os.Stderr, err.Error())
-		os.Exit(1)
-	}
-	fmt.Printf("            i32.const %d\n", n)
+	// Traverse the AST to emit assembly.
+	gen(node)
 
-	for !atEOF() {
-		if consume("+") {
-			n, err := expectNumber()
-			if err != nil {
-				fmt.Fprint(os.Stderr, err.Error())
-				os.Exit(1)
-			}
-
-			fmt.Printf("            i32.const %d\n", n)
-			fmt.Print("            i32.add\n")
-			continue
-		}
-
-		if err := expect("-"); err != nil {
-			fmt.Fprint(os.Stderr, err.Error())
-			os.Exit(1)
-		}
-		n, err := expectNumber()
-		if err != nil {
-			fmt.Fprint(os.Stderr, err.Error())
-			os.Exit(1)
-		}
-
-		fmt.Printf("            i32.const %d\n", n)
-		fmt.Print("            i32.sub\n")
-		continue
-	}
 	fmt.Print("        )\n")
 	fmt.Print("    )\n")
 	fmt.Print(")\n")
